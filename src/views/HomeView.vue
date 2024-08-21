ce3ee7d74db2d48cd98aebb3c69b487bbabab787
Muh-Awwal Fabowale <fabowalemuhawwal@gmail.com>
2024-08-21 12:00:00 +0000

    feat: logic to take in user input for pdf

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import { gsap } from 'gsap';
+import { TextPlugin } from 'gsap/TextPlugin';
 import axios from "axios";
 import { ref } from "vue";
 import { marked } from 'marked';
 const api = "https://delveai-backend.onrender.com";
+//https://delveai-backend.onrender.com
 
 const prompt = ref("");
 const answerPrompt = ref("") as any;
@@ -17,6 +20,12 @@ const pdfFile = ref("") as any
 const fileInput = ref(null) as any
 const fileName = ref("") as any
 
+const promptsend = ref(true)
+const pdfsend = ref(false)
+
+const formdata = new FormData()
+gsap.registerPlugin(TextPlugin)
+
 async function getPrompt() {
   overlay.value = true;
   answerSheet.value = false;
@@ -29,10 +38,17 @@ async function getPrompt() {
     answerPrompt.value = marked(res.data);
     speakValue.value = res.data.replaceAll("*", "")
     prompt.value = "";
+    const answerspace = document.getElementById('answerspace') as any;
+    answerspace.innerHTML = "";
+    gsap.to(answerspace, { duration: 10, text: answerPrompt.value })
     if (answerPrompt.value.length >= 1) {
       overlay.value = false;
       answerSheet.value = true;
       audioIcon.value = true;
+
+      //send button switch
+      promptsend.value = true;
+      pdfsend.value = false;
     }
   } catch (err) {
     console.log(err);
@@ -52,34 +68,67 @@ const triggerUpload = () => {
 
 const selectedFile = async (e: any) => {
   pdfFile.value = e.target.files[0]
-  const formdata = new FormData()
   formdata.append("file", e.target.files[0])
+  formdata.append("text", prompt.value)
   console.log(pdfFile.value)
   localStorage.setItem('pdf', e.target.files[0].name)
   fileName.value = e.target.files[0].name
 
+  if (e.target.files[0]) {
+    promptsend.value = false;
+    pdfsend.value = true;
+    fileName.value = e.target.files[0].name
+  } else {
+    promptsend.value = true;
+    pdfsend.value = false;
+  }
+}
+
+const usePdf = async () => {
   overlay.value = true;
   answerSheet.value = false;
 
   try {
-    const res = await axios.post(`${api}/pdf/pdf_summarize`, formdata)
+    const res = await axios.post(`${api}/pdf/pdf_interaction`, formdata)
     console.log(res.data)
     answerPrompt.value = marked(res.data);
     speakValue.value = res.data.replaceAll("*", "")
     prompt.value = "";
+
+    const answerspace = document.getElementById('answerspace') as any;
+    answerspace.innerHTML = "";
+    gsap.to(answerspace, { duration: 10, text: answerPrompt.value })
     if (answerPrompt.value.length >= 1) {
       overlay.value = false;
       answerSheet.value = true;
       audioIcon.value = true;
+
+      //send button switch
+      promptsend.value = true;
+      pdfsend.value = false;
     }
   } catch (err: any) {
     console.log(err.message)
+    promptsend.value = false;
+    pdfsend.value = true;
   }
 }
 
 const playAudiofText = async () => {
   //@ts-ignore
   responsiveVoice.speak(`${speakValue.value}`, "UK English Male", { volume: 1 }, { pitch: 2 }, { rate: 1.5 });
+}
+
+const refreshScreen = () => {
+  if (fileName.value !== '' && answerPrompt.value !== '') {
+    fileName.value = ""
+    answerPrompt.value = ""
+    answerSheet.value = false
+  } else if (fileName.value == '' && answerPrompt.value !== '') {
+    fileName.value = ""
+    answerPrompt.value = ""
+    answerSheet.value = false
+  }
 }
 </script>
 
@@ -93,7 +142,8 @@ const playAudiofText = async () => {
       <div
         class="input_container space-x-[30px] w-full mx-auto rounded-[4px] border border-[#303139] pl-[10px] py-[0px] bg-[#40414e]">
         <div class="w-[80%] h-fit">
-          <input placeholder="Enter your prompt..." autocomplete="off" v-model="prompt" id="inputai" type="text"
+          <input @keyup="refreshScreen" placeholder="Enter your prompt..." autocomplete="off" v-model="prompt"
+            id="inputai" type="text"
             class="w-full bg-[transparent] roboto-black text-[250] focus:outline-none text-[13px] text-[#fff] rounded-[16px] py-[15px] px-[20px] tracking-[1px]" />
         </div>
         <div class="w-[20%]">
@@ -111,7 +161,14 @@ const playAudiofText = async () => {
               <div class="w-[20px] h-[20px]">
                 <img @click="triggerUpload" src="/pdficon.svg" class="w-full h-full cursor-pointer" />
               </div>
-              <div class="cursor-pointer" @click="getPrompt">
+              <div v-show="promptsend" class="cursor-pointer" @click="getPrompt">
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                  <path
+                    d="M13.517 7.78232C13.6622 7.70957 13.7842 7.59787 13.8695 7.45972C13.9548 7.32156 14 7.1624 14 7.00004C14 6.83767 13.9548 6.67851 13.8695 6.54036C13.7842 6.4022 13.6622 6.2905 13.517 6.21775L1.26652 0.0924925C1.11462 0.0164901 0.943888 -0.013663 0.775147 0.00571153C0.606407 0.025086 0.446953 0.0931506 0.316236 0.201602C0.185518 0.310054 0.0891905 0.454204 0.0390005 0.616468C-0.0111885 0.778733 -0.0130692 0.952096 0.0335884 1.11541L1.28402 5.4906C1.33632 5.67346 1.44678 5.8343 1.59867 5.94877C1.75056 6.06324 1.93561 6.12511 2.1258 6.125L6.1256 6.125C6.35767 6.125 6.58024 6.21719 6.74434 6.38129C6.90844 6.54539 7.00063 6.76796 7.00063 7.00004C7.00063 7.23211 6.90844 7.45468 6.74434 7.61878C6.58024 7.78288 6.35767 7.87507 6.1256 7.87507L2.1258 7.87507C1.93561 7.87497 1.75056 7.93683 1.59867 8.0513C1.44678 8.16577 1.33632 8.32661 1.28402 8.50948L0.0344624 12.8847C-0.0122877 13.0479 -0.0105196 13.2213 0.0395511 13.3835C0.0896219 13.5458 0.185831 13.69 0.316447 13.7986C0.447062 13.9071 0.606437 13.9753 0.775136 13.9948C0.943837 14.0143 1.11457 13.9843 1.26652 13.9085L13.517 7.7832L13.517 7.78232Z"
+                    fill="#8E8E9E" />
+                </svg>
+              </div>
+              <div v-show="pdfsend" id="pdf_click" class="cursor-pointer" @click="usePdf">
                 <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
                   <path
                     d="M13.517 7.78232C13.6622 7.70957 13.7842 7.59787 13.8695 7.45972C13.9548 7.32156 14 7.1624 14 7.00004C14 6.83767 13.9548 6.67851 13.8695 6.54036C13.7842 6.4022 13.6622 6.2905 13.517 6.21775L1.26652 0.0924925C1.11462 0.0164901 0.943888 -0.013663 0.775147 0.00571153C0.606407 0.025086 0.446953 0.0931506 0.316236 0.201602C0.185518 0.310054 0.0891905 0.454204 0.0390005 0.616468C-0.0111885 0.778733 -0.0130692 0.952096 0.0335884 1.11541L1.28402 5.4906C1.33632 5.67346 1.44678 5.8343 1.59867 5.94877C1.75056 6.06324 1.93561 6.12511 2.1258 6.125L6.1256 6.125C6.35767 6.125 6.58024 6.21719 6.74434 6.38129C6.90844 6.54539 7.00063 6.76796 7.00063 7.00004C7.00063 7.23211 6.90844 7.45468 6.74434 7.61878C6.58024 7.78288 6.35767 7.87507 6.1256 7.87507L2.1258 7.87507C1.93561 7.87497 1.75056 7.93683 1.59867 8.0513C1.44678 8.16577 1.33632 8.32661 1.28402 8.50948L0.0344624 12.8847C-0.0122877 13.0479 -0.0105196 13.2213 0.0395511 13.3835C0.0896219 13.5458 0.185831 13.69 0.316447 13.7986C0.447062 13.9071 0.606437 13.9753 0.775136 13.9948C0.943837 14.0143 1.11457 13.9843 1.26652 13.9085L13.517 7.7832L13.517 7.78232Z"
@@ -132,7 +189,7 @@ const playAudiofText = async () => {
       </div>
       <div v-show="answerSheet"
         class="mt-[10px] answerContainer text-[15px] font-[500] h-[300px] overflow-y-scroll w-full mx-auto border pt-[26px] pb-[0px] pl-[30px] pr-[10px] rounded-[4px] border-[#32343a] inter-tight">
-        <div v-html="answerPrompt" class="leading-[27px] textPlace pb-[26px]"></div>
+        <div id="answerspace" class="leading-[27px] textPlace pb-[26px]"></div>
       </div>
       <!-- summarize input file pdf -->
       <form @submit="preventAutoRedirect" enctype="multipart/form-data">
