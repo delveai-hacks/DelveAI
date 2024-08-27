02163bc1e19b468b47b5bb82eb7d0541670c737e
Muh-Awwal Fabowale <fabowalemuhawwal@gmail.com>
2024-08-27 23:58:45 +0100

    feat: proper width sizing in px for auths screens

--- src/views/app/password/NewPassword.vue
+++ src/views/app/password/NewPassword.vue
@@ -11,13 +11,13 @@ import PasswordChange from '@/components/forms/PasswordChange.vue';
   </div>
   <!-- end first flex form -->
   <div
-   class="hidden md:block md:h-fit md:w-[calc(100%-488px)] border bg-[#fbf9f9] rounded-[24px] px-[24px] overflow-hidden">
+   class="hidden md:block md:h-fit md:w-[calc(100%-488px)] border bg-[#fbf9f9] rounded-[24px] px-[24px] overflow-hidden break-words whitespace-normal">
    <div
     class="text-[#101828] mb-[10] md:mb-[20px] md:w-fit md:mx-auto text-center text-[20px] md:text-[24px] font-[700] leading-[normal] tracking-[-0.4px] md:mt-[86px]">
     Convert text to speech
    </div>
    <div
-    class="text-[#667085] lg:w-[445px] lg:mx-auto text-[14px] md:text-[18px] font-[500] text-center leading-[normal] tracking-[-0.36px] mb-[16px] md:mb-[32px]">
+    class="text-[#667085] lg:w-full lg:mx-auto text-[14px] md:text-[18px] font-[500] text-center leading-normal tracking-[-0.36px] mb-[16px] md:mb-[32px] break-words whitespace-normal">
     Listen to your documents on the go! Convert text
     into natural-sounding speech and take your content
     with you wherever you are.
--- src/views/app/password/PasswordEmailVerify.vue
+++ src/views/app/password/PasswordEmailVerify.vue
@@ -11,13 +11,13 @@ import VerifyEmail from '@/components/forms/VerifyEmail.vue';
   </div>
   <!-- end first flex form -->
   <div
-   class="hidden md:block md:h-fit md:w-[calc(100%-488px)] border bg-[#fbf9f9] rounded-[24px] px-[24px] overflow-hidden">
+   class="hidden md:block md:h-fit md:w-[calc(100%-488px)] border bg-[#fbf9f9] rounded-[24px] px-[24px] break-words whitespace-normal">
    <div
     class="text-[#101828] mb-[10] md:mb-[20px] md:w-fit md:mx-auto text-center text-[20px] md:text-[24px] font-[700] leading-[normal] tracking-[-0.4px] md:mt-[86px]">
     Summarize PDFs in Seconds
    </div>
    <div
-    class="text-[#667085] lg:w-[445px] lg:mx-auto text-[14px] md:text-[14px] font-[500] text-center leading-[normal] tracking-[-0.36px] mb-[16px] md:mb-[32px]">
+    class="text-[#667085] lg:w-full lg:mx-auto text-[14px] md:text-[14px] font-[500] text-center leading-[normal] tracking-[-0.36px] mb-[16px] md:mb-[32px] break-words whitespace-normal">
     Transform lengthy documents into concise summaries. Save time and get straight to the point.
    </div>
    <div class="w-full md:h-full rounded-[18px] overflow-hidden">
